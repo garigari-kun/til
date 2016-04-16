cb80353d078e5b012328577c8232466bc89d616a
Keisuke Tsukamoto <keisuke.cs@gmail.com>
2016-04-16 09:46:21 +0900

    Modified args.py

--- src/basic/basic/args.py
+++ src/basic/basic/args.py
@@ -8,7 +8,12 @@ def simple2(a, *args):
 def simple3(**kwargs):
     print(kwargs)
 
+def simple4(*args, **kwargs):
+    print(args)
+    print(kwargs)
+
 if __name__ == '__main__':
     simple(1, 2, 3)
     simple2(1, 2, 3)
     simple3(x = 1, y = 2)
+    simple4(1, 2, 3, 4, 5, 6, 7, x = 2, y = 2, name = 'someone')
