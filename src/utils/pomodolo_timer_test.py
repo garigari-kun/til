3f4bab5d576bcd69d2e594f8f9f099c3e62df70b
kei-cs <keisuke.cs@gmail.com>
2016-05-02 22:53:18 +0900

    Modified pomodolo_timer_test.py

--- src/utils/pomodolo_timer_test.py
+++ src/utils/pomodolo_timer_test.py
@@ -1,40 +1,88 @@
 from datetime import datetime, timedelta
 from time import sleep
 
+# minutes
+POMODORO_WORK_TIME = 3
 
-def simple_timer():
+# minutes
+POMODORO_SHORT_BREAK_TIME = 3
+POMODORO_LONG_BREAK_TIME = 3
+
+# main
+# Run pomodoro function from here
+def main():
+    is_started = False
+    while not is_started:
+        check = input('Start working? Type "start": ')
+        if check == 'start':
+            is_started = True
+            # Call timer function
+            trigger_pomodoro_timer()
+        else:
+            is_started = False
+
+#
+def trigger_pomodoro_timer():
     now = datetime.now()
-    after_work = now + timedelta(seconds = 5)
+    after_work = now + timedelta(seconds = POMODORO_WORK_TIME)
     is_finished = False
+
     while not is_finished:
         is_finished = timer(after_work)
         sleep(1)
     if is_finished:
-        print('Well Done! You have 5 minitues of break time! Enjoy!')
-        break_time()
+        is_chosen = False
+        while not is_chosen:
+            print('Well Done!')
+            bt_kind = input('Short break or Long break?[short/long]: ')
+            if bt_kind == 'short':
+                is_chosen = True
+                trigger_short_break()
+            elif bt_kind == 'long':
+                is_chosen = True
+                trigger_long_break()
+            else:
+                is_chosen = False
+                print('Error. Choose again')
 
-def select_choices():
-    print('You have 3 options.')
-    print('b: Take a breaktime')
-    print('s: Start working again without breaktime')
-
-def break_time():
-    print('break time')
+#
+def trigger_short_break():
+    print('short break')
     is_finished = False
     now = datetime.now()
-    after_break = now + timedelta(seconds = 5)
+    after_break = now + timedelta(seconds = POMODORO_SHORT_BREAK_TIME)
     while not is_finished:
-        is_finished = start_breaktime(after_break)
+        is_finished = breaktimer(after_break)
         sleep(1)
+
     if is_finished:
-        choice = input("Start working?[y/n]: ")
+        choice = input("Start working? [y/n]: ")
         if choice == 'y':
-            simple_timer()
+            trigger_pomodoro_timer()
         else:
-            print('Thank you')
+            print('Good job.')
             return
 
-def start_breaktime(after_break):
+#
+def trigger_long_break():
+    print('long break')
+    is_finished = False
+    now = datetime.now()
+    after_break = now + timedelta(seconds = POMODORO_LONG_BREAK_TIME)
+    while not is_finished:
+        is_finished = breaktimer(after_break)
+        sleep(1)
+
+    if is_finished:
+        choice = input("Start working? [y/n]: ")
+        if choice == 'y':
+            trigger_pomodoro_timer()
+        else:
+            print('Good job.')
+            return
+
+#
+def breaktimer(after_break):
     is_finished = False
     moment = datetime.now()
     print("break: {}".format(moment))
@@ -43,6 +91,7 @@ def start_breaktime(after_break):
 
     return is_finished
 
+#
 def timer(after_work):
     is_finished = False
     moment = datetime.now()
@@ -54,8 +103,4 @@ def timer(after_work):
 
 
 if __name__ == '__main__':
-    whether_start = input('Start working? Press Enter!')
-    print(whether_start)
-    #simple_timer()
-    if not len(whether_start):
-        simple_timer()
+    main()
