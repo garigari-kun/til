dad90c982dac42d1718a7bdb6dc935420de00972
kei-cs <keisuke.cs@gmail.com>
2016-10-18 22:47:56 +0900

    2016/10/18

--- src/networking/FOPNP/ch1/search3.py
+++ src/networking/FOPNP/ch1/search3.py
@@ -6,7 +6,12 @@ base = '/maps/api/geocode/json'
 
 def geocode(address):
     path = '{}?address={}&sensor=false'.format(base, quote_plus(address))
-    print(path)
+    connection = http.client.HTTPConnection('maps.google.com')
+    connection.request('GET', path)
+    rawreply = connection.getresponse().read()
+    reply = json.loads(rawreply.decode('utf-8'))
+    print(reply['results'][0]['geometry']['location'])
+    
 
 
 if __name__ == '__main__':
