92b4b0c1c9614df97dde546f8f48b28252c23d2c
kei-cs <keisuke.cs@gmail.com>
2017-03-21 21:08:48 +0900

    2017-03-21

---
+++ src/django_practice/zen/src/shortener/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from .models import ZenURL
+
+
+admin.site.register(ZenURL)
---
+++ src/django_practice/zen/src/shortener/models.py
@@ -0,0 +1,42 @@
+
+from django.db import models
+
+from shortener.utils import code_generator, create_shortcode
+
+
+
+class ZenURLManager(models.Manager):
+    def all(self, *args, **kwargs):
+        qs_main = super(ZenURLManager, self).all(*args, **kwargs)
+        qs = qs_main.filter(active=True)
+        return qs
+
+    def refresh_shortcodes(self):
+        qs = ZenURL.objects.filter(id__gte=1)
+        new_codes = 0
+        for q in qs:
+            q.shortcode = create_shortcode(q)
+            q.save()
+            new_codes += 1
+        return 'New codes made: {}'.format(new_codes)
+
+
+
+
+class ZenURL(models.Model):
+    url = models.CharField(max_length=220)
+    shortcode = models.CharField(max_length=15, unique=True, blank=True)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=True)
+
+    objects = ZenURLManager()
+
+    def __str__(self):
+        return str(self.url)
+
+
+    def save(self, *args, **kwargs):
+        if self.shortcode is None or self.shortcode == '':
+            self.shortcode = create_shortcode(self)
+        super(ZenURL, self).save(*args, **kwargs)
