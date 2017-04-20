0111db25ced318ea711a742b7f951292c1cba220
kei-cs <keisuke.cs@gmail.com>
2017-04-20 23:01:45 +0900

    2017-04-20

--- src/django_practice/try-django19/src/posts/urls.py
+++ src/django_practice/try-django19/src/posts/urls.py
@@ -5,7 +5,7 @@ from .views import post_list, post_create, post_update, post_delete, post_detail
 urlpatterns = [
     url(r'^$', post_list),
     url(r'^create/$', post_create),
-    url(r'^update/$', post_update),
+    url(r'^(?P<id>\d+)/update$', post_update, name='update'),
     url(r'^delete/$', post_delete),
     url(r'^(?P<id>\d+)/$', post_detail, name='detail'),
 
--- src/django_practice/try-django19/src/posts/views.py
+++ src/django_practice/try-django19/src/posts/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 
 from .forms import PostModelForm
@@ -22,15 +22,32 @@ def post_create(request):
         if form.is_valid():
             instance = form.save(commit=False)
             instance.save()
+            return HttpResponseRedirect(instance.get_absolute_url())
 
     context = {
+        'title': 'Create',
         'form': form
     }
     return render(request, template_name, context)
-    # return HttpResponse('Create')
 
-def post_update(request):
-    return HttpResponse('Update')
+
+def post_update(request, id=None):
+    template_name = 'post_form.html'
+    instance = get_object_or_404(Post, id=id)
+    form = PostModelForm(request.POST or None, instance=instance)
+    if request.method == 'POST':
+        if form.is_valid():
+            instance = form.save(commit=False)
+            instance.save()
+            return HttpResponseRedirect(instance.get_absolute_url())
+
+    context = {
+        'title': 'Update',
+        'form': form
+    }
+    return render(request, template_name, context)
+
+
 
 def post_delete(request):
     return HttpResponse('Delete')
