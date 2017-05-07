06e7876453f6324e068bf9b8b8a31a40cae4b407
kei-cs <keisuke.cs@gmail.com>
2017-05-07 21:04:17 +0900

    2017-05-07

--- src/django_practice/knowledge/src/formset_sample/views.py
+++ src/django_practice/knowledge/src/formset_sample/views.py
@@ -1,3 +1,16 @@
+from django.forms import formset_factory
 from django.shortcuts import render
+from django.views.generic.base import View
 
-# Create your views here.
+from .forms import LinkForm
+
+
+class CreateLinkView(View):
+    def get(self, request, *args, **kwargs):
+        LinkFormSet = formset_factory(LinkForm)
+        formset = LinkFormSet()
+        context = {
+            'formset': formset,
+        }
+        template_name = 'formset.html'
+        return render(request, template_name, context)
