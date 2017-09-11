6d3abaeddef27f4c170fb06b0dac29c4220b5dbd
kei-cs <keisuke.cs@gmail.com>
2017-09-11 22:28:33 +0900

    2017-09-11

--- src/textbooks/grokking_algo/ch7/dijkstra.py
+++ src/textbooks/grokking_algo/ch7/dijkstra.py
@@ -7,6 +7,43 @@
 
 
 
-graph[“start”] = {}
-graph[“start”][“a”] = 6
-graph[“start”][“b”] = 2
+graph['start'] = {}
+graph['start']['a'] = 6
+graph['start']['b'] = 2
+
+graph['a'] = {}
+graph['a']['fin'] = 1
+
+graph['b'] = {}
+graph['b']['a'] = 3
+graph['b']['fin'] = 5
+
+graph['fin'] = {}
+
+
+infinity = float('inf')
+costs = {}
+costs['a'] = 6
+costs['b'] = 2
+costs['fin'] = infinity
+
+parents = {}
+parents['a'] = 'start'
+parents['b'] = 'start'
+parents['fin'] = None
+
+processed = []
+
+
+node = find_lowest_cost_node(costs)
+while node is not None:
+    cost = costs[node]
+    neighbors = graph[node]
+    for n in neighbors.keys():
+        new_cost = cost + neighbors[n]
+        if costs[n] > new_cost:
+            costs[n] = new_cost
+            parents[n] = node
+
+    processed.append(node)
+    cost = find_lowest_cost_node(costs)
