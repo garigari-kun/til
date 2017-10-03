669112332661c45f4403837e1f31499cdefad64a
kei-cs <keisuke.cs@gmail.com>
2017-10-03 23:13:04 +0900

    2017-10-03

---
+++ src/codewars/python/8kyu/find_needle.py
@@ -0,0 +1,45 @@
+"""
+
+
+Can you find the needle in the haystack?
+
+Write a function findNeedle() that takes an array full of junk but containing one "needle"
+
+After your function finds the needle it should return a message (as a string) that says:
+
+"found the needle at position " plus the index it found the needle
+
+So
+
+find_needle(['hay', 'junk', 'hay', 'hay', 'moreJunk', 'needle', 'randomJunk'])
+find_needle(['hay', 'junk', 'hay', 'hay', 'moreJunk', 'needle', 'randomJunk'])
+findNeedle(['hay', 'junk', 'hay', 'hay', 'moreJunk', 'needle', 'randomJunk'])
+findNeedle(new Object[] {"hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"})
+find_needle(["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"])
+should return
+
+'found the needle at position 5'
+'found the needle at position 5'
+'found the needle at position 5'
+"found the needle at position 5"
+"found the needle at position 5"
+
+
+
+
+"""
+
+
+def find_needle(haystack):
+    # your code here
+    target_str = 'needle'
+    for index, word in enumerate(haystack):
+        if str(word) == target_str:
+            return 'found the needle at position ' + str(index)
+
+    return False
+
+
+if __name__ == '__main__':
+    print(find_needle(['hay', 'junk', 'hay', 'hay', 'moreJunk', 'needle', 'randomJunk']))
+    print(find_needle(["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"]))
