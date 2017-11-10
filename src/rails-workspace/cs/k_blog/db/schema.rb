b152d80e29ecb71e4d744481cf6da7acba80db68
kei-cs <keisuke.cs@gmail.com>
2017-11-10 22:43:58 +0900

    2017-11-10

--- src/rails-workspace/cs/k_blog/db/schema.rb
+++ src/rails-workspace/cs/k_blog/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171109121341) do
+ActiveRecord::Schema.define(version: 20171110125042) do
 
   create_table "movies", force: true do |t|
     t.string   "name"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 20171109121341) do
     t.string   "format"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image"
+    t.string   "thumbnail"
   end
 
   create_table "people", force: true do |t|
--- src/rails-workspace/cs/k_blog/db/seeds.rb
+++ src/rails-workspace/cs/k_blog/db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+# t.string   "name"
+# t.string   "director"
+# t.text     "description"
+# t.integer  "year"
+# t.decimal  "length"
+# t.string   "format"
+# t.datetime "created_at"
+# t.datetime "updated_at"
+# t.string   "image"
+# t.string   "thumbnail"
+
+
+movie = Movie.create(
+  :name => "Machine",
+  :director => "Johnson",
+  :description => "Something about machine",
+  :year => 2017,
+  :length => 120,
+  :format => "HD",
+)
