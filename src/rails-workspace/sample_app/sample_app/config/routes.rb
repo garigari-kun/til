9e0ae5eb49acb52070b285fc89fde1d2fedb5317
kei-cs <keisuke.cs@gmail.com>
2018-03-21 23:43:46 +0900

    2018-03-21

--- src/rails-workspace/sample_app/sample_app/config/routes.rb
+++ src/rails-workspace/sample_app/sample_app/config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   get '/contact', :to => "static_pages#contact"
   # users
   get 'signup', :to => "users#new"
-
+  resources :users
+  post 'signup', :to => "users#create"
 end
