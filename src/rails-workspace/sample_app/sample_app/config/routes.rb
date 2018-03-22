24000ab37d8926ae3051fb80070eb0e6e30716b0
kei-cs <keisuke.cs@gmail.com>
2018-03-22 23:54:39 +0900

    2018-03-22

--- src/rails-workspace/sample_app/sample_app/config/routes.rb
+++ src/rails-workspace/sample_app/sample_app/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   root 'static_pages#home'
   get '/home', :to => "static_pages#home"
   get '/help', :to => "static_pages#help"
@@ -8,4 +10,9 @@ Rails.application.routes.draw do
   get 'signup', :to => "users#new"
   resources :users
   post 'signup', :to => "users#create"
+
+  # sessions
+  get '/login', :to => 'sessions#new'
+  post '/login', :to => 'sessions#create'
+  delete '/logout', :to => 'sessions#destroy'
 end
