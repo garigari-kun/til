9be804c18c83a562f829fbd0ca4b9ead25d96464
kei-cs <keisuke.cs@gmail.com>
2018-04-02 23:57:29 +0900

    2018-04-02

--- src/textbooks/rails4_in_action/ticketee/ticketee/app/controllers/projects_controller.rb
+++ src/textbooks/rails4_in_action/ticketee/ticketee/app/controllers/projects_controller.rb
@@ -3,5 +3,30 @@ class ProjectsController < ApplicationController
   def index
   end
 
+  def show
+    @project = Project.find(params[:id])
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+
+    if @project.save
+      flash[:notice] = "Project has been created."
+      redirect_to project_path(@project.id)
+    else
+      # rerender new
+    end
+  end
+
+  private
+
+  def project_params
+    return params.require(:project).permit(:name, :description)
+  end
+
 
 end
