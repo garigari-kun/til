7c7ceecc5a14cd243f82a595de83f3a9f5ea745a
kei-cs <keisuke.cs@gmail.com>
2018-04-05 23:51:11 +0900

    2018-04-05

--- src/textbooks/rails4_in_action/ticketee/ticketee/app/controllers/projects_controller.rb
+++ src/textbooks/rails4_in_action/ticketee/ticketee/app/controllers/projects_controller.rb
@@ -1,11 +1,12 @@
 class ProjectsController < ApplicationController
 
+  before_action :set_project, only: [:show, :edit, :update, :destroy]
+
   def index
     @projects = Project.all
   end
 
   def show
-    @project = Project.find(params[:id])
   end
 
   def new
@@ -25,11 +26,9 @@ class ProjectsController < ApplicationController
   end
 
   def edit
-    @project = Project.find(params[:id])
   end
 
   def update
-    @project = Project.find(params[:id])
     if @project.update(project_params)
       flash[:notice] = "Project has been updated."
       redirect_to project_path(@project.id)
@@ -40,9 +39,9 @@ class ProjectsController < ApplicationController
   end
 
   def destroy
-    @project = Project.find(params[:id])
     @project.destroy
 
+    flash[:notice] = "Project has been deleted."
     redirect_to projects_path
   end
 
@@ -51,6 +50,15 @@ class ProjectsController < ApplicationController
 
   def project_params
     return params.require(:project).permit(:name, :description)
+  end
+
+  def set_project
+    begin
+      @project = Project.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      flash[:alert] = "The project you were looking for could not be found."
+      redirect_to projects_path
+    end
   end
 
 
