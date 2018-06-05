40b7d9a1c92868f7b8d454c2349ff2c22e27334c
kei-cs <keisuke.cs@gmail.com>
2018-06-06 00:08:40 +0900

    cleaned up

--- src/vue_practice/djangovuejs/src/joblist/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class JobList(models.Model):
-    jobtitle = models.CharField(max_length=100)
-    jobdescription = models.CharField(max_length=300)
-    postdate = models.DateField(auto_now_add=True)
--- src/vue_practice/djangovuejs/src/joblist/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-
-from .models import JobList
-
-class JobListSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = JobList
-        fields = ('id', 'jobtitle', 'jobdescription', 'postdate')
