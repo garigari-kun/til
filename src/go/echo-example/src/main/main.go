26fc3c24f99f325ba2444270ed720bd7fbff4002
kei-cs <keisuke.cs@gmail.com>
2018-08-16 18:53:44 +0900

    2018-08-14

--- src/go/echo-example/src/main/main.go
+++ src/go/echo-example/src/main/main.go
@@ -2,8 +2,45 @@ package main
 
 import (
   "fmt"
+  "net/http"
+  "github.com/labstack/echo"
 )
 
+// /
+func yallo(c echo.Context) error {
+  return c.String(http.StatusOK, "yallo page")
+}
+
+// /cats/:data
+func getCats(c echo.Context) error {
+  catName := c.QueryParam("name")
+  catType := c.QueryParam("type")
+
+  dataType := c.Param("data")
+
+  if dataType == "string" {
+    return c.String(http.StatusOK, fmt.Sprintf("Cat name is %s and type is %s", catName, catType))
+  } else if dataType == "json" {
+    return c.JSON(http.StatusOK, map[string]string {
+      "name": catName,
+      "type": catType,
+    })
+  } else {
+    return c.JSON(http.StatusBadRequest, map[string]string {
+      "error": "You must specify what data type is you want.",
+    })
+  }
+
+}
+
+
 func main() {
-  fmt.Println("this is the main go file.")
+  fmt.Println("Start Fusuma.....")
+
+  e := echo.New()
+
+  e.GET("/", yallo)
+  e.GET("/cats/:data", getCats)
+
+  e.Start(":3000")
 }
